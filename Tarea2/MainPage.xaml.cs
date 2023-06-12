5dcb8826a5fcde0fe263a1e1464f087ba1468671
maplicano <maplicano@DESKTOP-CFG8MP5>
2023-06-11 20:23:10 -0600

    Agregar archivos de proyecto.

---
+++ Tarea2/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public class UserData
+        {
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public string Edad { get; set; }
+            public string Correo { get; set; }
+
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var userData = new UserData
+            {
+                Nombre = nombre.Text,
+                Apellido = apellido.Text,
+                Edad = edad.Text,
+                Correo = correo.Text
+            };
+
+            Navigation.PushAsync(new Page1
+            {
+                BindingContext = userData
+            });
+        }
+
+    }
+}
---
+++ Tarea2/Page1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static Tarea2.MainPage;
+
+namespace Tarea2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var userData = BindingContext as UserData;
+
+            if (userData != null)
+            {
+                // Accede a las propiedades del objeto userData
+                var nombre = userData.Nombre;
+                var apellido = userData.Apellido;
+                var edad = userData.Edad;
+                var correo = userData.Correo;
+                // Haz lo que necesites con los datos ingresados por el usuario
+                // ...
+            }
+        }
+    }
+}
